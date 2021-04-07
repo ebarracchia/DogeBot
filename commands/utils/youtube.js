d5147f8c5ddaafc9aeb5ba0cb271c06c3d4790e7
Ernesto Barracchia <barracchiae@globalhitss.com>
2021-04-07 09:06:33 -0300

    Add youtube search

--- commands/utils/youtube.js
+++ commands/utils/youtube.js
@@ -1,5 +1,20 @@
 const youtubesearchapi=require('youtube-search-api');
 
+const getYoutubeInfo = (item) => {
+    switch (item.type) {
+        case 'video':
+            return `${item.title} | ⏰ (${item.length.simpleText}) - 🔗 https://www.youtube.com/watch?v=${item.id}`;
+            break;
+        case 'channel':
+            return `${item.title} - 🔗 https://www.youtube.com/channel/${item.id}`;
+            break;
+        
+        default:
+            return '';
+            break;
+    }
+}
+
 module.exports = {
     name: 'youtube',
     description: 'search into youtube',
@@ -11,8 +26,7 @@ module.exports = {
             const result = await youtubesearchapi.GetListByKeyword(args, false);
             if (result.items.length) {
                 const item = result.items[0];
-                console.log(item);
-                msgObj = [`${item.title} - https://www.youtube.com/watch?v=${item.id}`];
+                msgObj = [`${getYoutubeInfo(item)}`];
             } else {
                 msgObj = [`No results.`];
             }
